dd9a106785b95425d545d858a22f1f7762951640
Luuk <luuksteusfij@gmail.com>
2025-06-18 01:54:21 +0200

    Added endpoints

--- TrendGuideAPI/Controllers/TrendsController.cs
+++ TrendGuideAPI/Controllers/TrendsController.cs
@@ -1,3 +1,4 @@
+using FunnelTrendRadarAPI.Models.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,6 +15,11 @@ namespace TrendGuideAPI.Controllers
             _context = context;
         }
 
+        private bool TrendExists(long id)
+        {
+            return _context.Trend.Any(e => e.Id == id);
+        }
+
         [HttpGet(Name = "GetTrends")]
         public async Task<IActionResult> GetTrends()
         {
@@ -23,6 +29,72 @@ namespace TrendGuideAPI.Controllers
                 return NotFound("No trends found in the database");
             }
             return Ok(trends);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Trend>> GetTrend(int id)
+        {
+            var trend = await _context.Trend.FindAsync(id);
+
+            if (trend == null)
+            {
+                return NotFound();
+            }
+
+            return trend;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Trend>> PostTrend(Trend trend)
+        {
+            _context.Trend.Add(trend);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTrends), new { id = trend.Id }, trend);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTrend(long id, Trend trend)
+        {
+            if (id != trend.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(trend).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TrendExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTrend(int id)
+        {
+            var trend = await _context.Trend.FindAsync(id);
+            if (trend == null)
+            {
+                return NotFound();
+            }
+
+            _context.Trend.Remove(trend);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
--- TrendGuideAPI/Program.cs
+++ TrendGuideAPI/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
         policy =>
         {
-            policy.WithOrigins("http://localhost:8081");
+            policy.WithOrigins("http://localhost:8081").AllowAnyHeader().AllowAnyMethod();
         });
 });
 
